5de6e86f2bee107fc9126e254a61cb3e57195d7a
Jesus Moreno <jesusrmor94@gmail.com>
2016-06-17 21:09:45 -0500

    Mutation based rather than interval based

--- inject.js
+++ inject.js
@@ -1,6 +1,19 @@
 (function() {
-  $('article[data-source*=reading_list---------]').parent().parent().parent().remove();
-  setInterval(function() {
-    $('article[data-source*=reading_list---------]').parent().parent().parent().remove();
-  }, 10);
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  $('article[data-source*=reading_list---------]')
+    .parent()
+    .parent()
+    .parent()
+    .remove();
+  var observer = new MutationObserver(function(mutations, observer) {
+    $('article[data-source*=reading_list---------]')
+      .parent()
+      .parent()
+      .parent()
+      .remove();
+  });
+  observer.observe(document, {
+    subtree: true,
+    attributes: true,
+  });
 })();
