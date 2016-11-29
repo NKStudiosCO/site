e739018fbc26a25de0117ab2c770abf81d9bdaaa
Nikolas Karinja <nikobikoliko@gmail.com>
2016-11-29 07:51:50 -0800

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-// listen for our browerAction to be clicked
-chrome.webNavigation.onCompleted.addListener(function (tab) {
-	chrome.tabs.executeScript(tab.ib, { file: 'jquery.js' }, function() {
-    chrome.tabs.executeScript(tab.ib, { file: 'inject.js' });
-  });
-});
